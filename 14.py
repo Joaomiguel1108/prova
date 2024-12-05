d21ce9f7e1ad859472c3393128a582b947c7a9af
João Miguel <joaomiguelmorais1108@gmail.com>
2024-12-04 21:23:54 -0300

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,12 @@
+senha = 0 
+
+while senha != 123:
+    senhaa = int(input("Digite a senha"))
+    
+    if senhaa == 123:
+        print("senha correta")
+        senha == 123
+        break
+    
+    if senhaa != 123:
+        print("senha incorreta")
